0f00add1baac6c07aee9552ce5b772308efef1c6
Joshua Gomez <jngomez@gwu.edu>
2013-03-28 11:06:52 -0400

    Added Digg-style pagination for items in the collection page. Closes #64 TODO: add same style to projects pages

---
+++ inv/invapp/utils.py
@@ -0,0 +1,42 @@
+def build_digg_style_boxes(items):
+    tp = items.paginator.num_pages
+    # build list of boxes (11 inner boxes + 2 arrows)
+    boxes = list(range(13))
+
+    # Add the backward and forward arrows
+    boxes[0] = {'disp': '<<', 'link': str(items.previous_page_number())}
+    boxes[0]['disabled'] = not items.has_previous
+    boxes[12] = {'disp': '>>', 'link': str(items.next_page_number())}
+    boxes[12]['disabled'] = not items.has_next
+
+    # Add the first and last numbers
+    boxes[1] = {'disp': '1', 'link': '1'}
+    boxes[1]['disabled'] = items.number == 1
+    boxes[11] = {'disp': tp, 'link': str(tp)}
+    boxes[11]['disabled'] = items.number == tp
+
+    # Add rest of numbers
+
+    # if small, don't bother with left ellipsis
+    if items.number < 7:
+        for x in range(2, 11):
+            boxes[x] = {'disp': str(x), 'link': str(x)}
+            boxes[x]['disabled'] = items.number == x
+        boxes[10] = {'disp': '...', 'link': None, 'disabled': True}
+    # if large, don't bother with right ellipsis
+    elif items.number > tp - 6:
+        boxes[2] = {'disp': '...', 'link': None, 'disabled': True}
+        num = tp - 9
+        for x in range(3, 11):
+            num += 1
+            boxes[x] = {'disp': str(num), 'link': str(num)}
+            boxes[x]['disabled'] = items.number == num
+    else:
+        boxes[2] = {'disp': '...', 'disabled': True}
+        for x in range(-3, 4):
+            boxes[x+6] = {'disp': str(items.number + x),
+                'link': str(items.number + x)}
+            boxes[x+6]['disabled'] = x == 0
+        boxes[10] = {'disp': '...', 'link': None, 'disabled': True}
+
+    return boxes
--- inv/invapp/views.py
+++ inv/invapp/views.py
@@ -4,6 +4,9 @@ from invapp.models import Collection, Project, Item, Bag, BagAction
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from utils import build_digg_style_boxes
+
+
 def collection(request, id):
     collection = get_object_or_404(Collection, id=id)
     projects = Project.objects.filter(collection=collection).defer('collection',
@@ -20,6 +23,7 @@ def collection(request, id):
             items = items_paginator.page(1)
         except EmptyPage:
             items = items_paginator.page(items_paginator.num_pages)
+        items.boxes = build_digg_style_boxes(items)
     return render(request, 'collection.html',
         {'collection': collection, 'projects': projects, 'items': items})
 
