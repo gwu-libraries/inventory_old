0a1454c42b6a49a6a40d9512f4f47a57c4810b39
Joshua Gomez <jngomez@gwu.edu>
2013-04-24 13:47:33 -0400

    Several small but significant changes. Now using JSONField to store Bag objects data instead of text blob. Rewrote Bag models stats function. Removed use of empty_stats template as it was easy to use improperly and create common pointers instead of new copies. Migrated database to new fields. Fixes #71

--- inv/invapp/models.py
+++ inv/invapp/models.py
@@ -12,9 +12,6 @@ from invapp.utils import merge_dicts
 models.signals.post_save.connect(create_api_key, sender=User)
 
 
-empty_stats = {'total_count': 0, 'total_size': 0, 'types': {}}
-
-
 class Machine(models.Model):
     name = models.CharField(max_length=64, unique=True)
     url = models.URLField(unique=True)
@@ -30,14 +27,14 @@ class Collection(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.stats:
-            self.stats = empty_stats
+            self.stats = {'total_count': 0, 'total_size': 0, 'types': {}}
         super(Collection, self).save(*args, **kwargs)
 
     def collect_stats(self):
         if self.items.count():
             return reduce(merge_dicts,
                 map(lambda item: item.stats, self.items.all()))
-        return empty_stats
+        return {'total_count': 0, 'total_size': 0, 'types': {}}
 
     def purl(self):
         return '%s/%s' % (settings.ID_SERVICE_URL, self.id)
@@ -55,14 +52,14 @@ class Project(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.stats:
-            self.stats = empty_stats
+            self.stats = {'total_count': 0, 'total_size': 0, 'types': {}}
         super(Project, self).save(*args, **kwargs)
 
     def collect_stats(self):
         if self.items.count():
             return reduce(merge_dicts,
                 map(lambda item: item.stats, self.items.all()))
-        return empty_stats
+        return {'total_count': 0, 'total_size': 0, 'types': {}}
 
 
 
@@ -86,14 +83,14 @@ class Item(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.stats:
-            self.stats = empty_stats
+            self.stats = {'total_count': 0, 'total_size': 0, 'types': {}}
         super(Item, self).save(*args, **kwargs)
 
     def collect_stats(self):
         if self.bags.count():
             return reduce(merge_dicts,
-                map(lambda bag: bag.pstats(), self.bags.all()))
-        return empty_stats
+                map(lambda bag: bag.stats, self.bags.all()))
+        return {'total_count': 0, 'total_size': 0, 'types': {}}
 
     def purl(self):
         return '%s/%s' % (settings.ID_SERVICE_URL, self.id)
@@ -106,8 +103,8 @@ class Bag(models.Model):
     machine = models.ForeignKey(Machine, related_name='bags')
     path = models.URLField()
     bag_type = models.CharField(max_length=1, choices=settings.BAG_TYPES)
-    payload_raw = models.TextField(blank=True)
-    payload_stats = models.TextField(blank=True)
+    payload = models.TextField(blank=True)
+    stats = JSONField()
     '''
     lines in payload should be formatted as such:
     relative_filepath_from_bag_directory file_size(MB)\n
@@ -116,38 +113,32 @@ class Bag(models.Model):
     def urlpath(self):
         return '%s/%s' % (self.machine.url.rstrip('/'), self.path.lstrip('/'))
 
-    def payload(self):
-        return [line.split() for line in self.payload_raw.split('\n') if line]
-
-    def pstats(self):
-        return json.loads(self.payload_stats)
+    def list_payload(self):
+        return [line.split() for line in self.payload.split('\n') if line]
 
     def filecount(self):
-        return self.pstats()['total_count']
+        return self.stats['total_count']
 
     def size(self):
-        return self.pstats()['total_size']
+        return self.stats['total_size']
 
-    def calc_pstats(self):
-        json = {
-            'total_count': 0,
-            'total_size': 0,
-            'types': {},
-        }
-        for f in self.payload():
-            fpath, fsize = f[0], f[1]
-            ftype = fpath.split('.')[-1].lower()
-            json['total_count'] += 1
-            json['total_size'] += int(fsize)
-            if ftype not in json['types'].keys():
-                json['types'][ftype] = {'count': 1, 'size': int(fsize)}
-            else:
-                json['types'][ftype]['count'] += 1
-                json['types'][ftype]['size'] += int(fsize)
-        return json
+    def collect_stats(self):
+        stats = {'total_count': 0, 'total_size': 0, 'types': {}}
+        if self.payload:
+            for fpath, fsize in self.list_payload():
+                ftype = fpath.split('.')[-1].lower() if '.' in fpath else 'unk'
+                stats['total_count'] += 1
+                stats['total_size'] += int(fsize)
+                if ftype not in stats['types'].keys():
+                    stats['types'][ftype] = {'count': 1, 'size': int(fsize)}
+                else:
+                    stats['types'][ftype]['count'] += 1
+                    stats['types'][ftype]['size'] += int(fsize)
+        return stats
 
     def save(self, *args, **kwargs):
-        self.payload_stats = json.dumps(self.calc_pstats())
+        if not self.stats:
+            self.stats = {'total_count': 0, 'total_size': 0, 'types': {}}
         super(Bag, self).save(*args, **kwargs)
 
 
