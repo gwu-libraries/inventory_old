a4c23776ebc967dfdb9716a5c2391042239abbad
Joshua <jngomez@gwu.edu>
2013-06-20 10:04:33 -0400

    Removed manual registration of ApiKey model. It is already discovered by the autodiscover function. Fixes #105

--- inv/invapp/admin.py
+++ inv/invapp/admin.py
@@ -5,9 +5,6 @@ from tastypie.models import ApiAccess, ApiKey
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-admin.site.register(ApiKey)
-admin.site.register(ApiAccess)
-
 
 class UserModelAdmin(UserAdmin):
     inlines = UserAdmin.inlines + [ApiKeyInline]
