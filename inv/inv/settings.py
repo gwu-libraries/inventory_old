fffadad9b3290076b43c8f3fc8689dcf6126be7d
Daniel Chudnov <dchud@gwu.edu>
2013-08-21 01:13:05 -0400

    more UI tweaks, blending into collection, bag, home pages, closes #165

--- inv/inv/settings.py
+++ inv/inv/settings.py
@@ -128,10 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'invapp',
     'south',
     'tastypie',
